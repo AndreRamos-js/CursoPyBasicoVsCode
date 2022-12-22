4628b215e1ac5f16e67743a59dade7a2978c5561
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-12-22 14:27:34 -0300

    Operadores not e not in

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -7,3 +7,6 @@ if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
     print('Entrar')
 else:
     print('Sair')
+
+print('lap' not in senha_digitada)
+print('lap' in senha_digitada)
