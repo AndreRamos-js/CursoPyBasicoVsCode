8af1166ea0e43bf99ad82686a811c44ed0bd03c0
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-12-29 15:42:18 -0300

    Exercicio 3

--- exercico_03.py
+++ exercico_03.py
@@ -1,6 +1,23 @@
-""""
-Faça um programa que peça ao usuário para digitar um número inteiro,
-informe se este número é par ou ímpar. Caso o usuário não digite um número
-inteiro, informe que não é um número inteiro.
+"""
+Faça um programa que pergunte a hora ao usuário (em números inteiros) e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
+
+horas = input('Digite as horas: ')
+
+if horas.isdigit():
+    horas_int = int(horas)
+    
+    if horas_int >= 0 and horas_int <= 11:
+        print('Bom dia!')
+    elif horas_int >= 12 and horas_int <=17:
+        print('Boa tarde!')
+    elif horas_int >= 18 and horas_int <= 23:
+        print('Boa noite!')
+    else:
+        print('Digite um número entre 0 e 23')
+else:
+    print('Você deve digitar as horas em números inteiros.')
+    print('Exemplo: 12')
