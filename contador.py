be344c923030d07aefa9cf3d0e2c82f652295fd6
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2022-12-31 20:10:55 -0300

    while + continue
    Implementando continue dentro do while.

--- contador.py
+++ contador.py
@@ -1,9 +1,28 @@
 
-
+"""
 contador = 0
 
 while contador < 20:
     contador = contador + 1
     print(contador)
+
+print('Acabou!')
+"""
+
+contador = 0
+
+while contador <= 100:
+    contador = contador + 1
+    
+    if contador == 6:
+        continue
+
+    if contador >= 10 and contador <= 27:
+        continue
+
+    print(contador)
+
+    if contador == 40:
+        break
 
 print('Acabou!')
