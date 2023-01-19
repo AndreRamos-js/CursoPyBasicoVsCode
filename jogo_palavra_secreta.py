1d8fc7d1121d991934b2d73d6f2e6bb84ae4a09f
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-01-19 18:34:22 -0300

    Finalização do jogo da Palavra Secreta

--- jogo_palavra_secreta.py
+++ jogo_palavra_secreta.py
@@ -3,13 +3,16 @@
 
 palavra_secreta = 'python'
 letras_acertadas = ''
+numero_tentativas = 0
 
 while True:
     letra_digitada = input('Digite uma letra: ')
-    
+    numero_tentativas += 1
+
     if len(letra_digitada) > 1:
         print('Digite apenas uma letra!')
         continue
+    
     if not letra_digitada.isalpha():
         print('Digite apenas letras!')
         continue
@@ -17,8 +20,28 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
+    palavra_formada = ''
+
     for letra in palavra_secreta:
         if letra in letras_acertadas:
-            print(letra)
+            palavra_formada += letra
         else:
-            print('*')
+            palavra_formada += '*'
+    
+    print(f'Palavra formada: {palavra_formada}')
+
+    if numero_tentativas == 20:
+        print(f'*' * 30)
+        print('VOCÊ PERDEU!')
+        print('O número de tentativas acabou.')
+        print(f'A palavra secreta era: {palavra_secreta}')
+        print(f'*' * 30)
+        break
+
+    if palavra_formada == palavra_secreta:
+        print(f'*' * 30)
+        print('PARABÉNS VOCê GANHOU!')
+        print(f'A palavra secreta era: {palavra_secreta}')
+        print(f'Tentativas: {numero_tentativas}')
+        print(f'*' * 30)
+        break
