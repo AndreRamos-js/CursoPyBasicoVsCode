6d2e56123449406f1637b3478895569585c00fa4
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-05-10 17:23:57 -0300

    Update split_join_e_strip.py

--- split_join_e_strip.py
+++ split_join_e_strip.py
@@ -14,3 +14,27 @@ for i, frase in enumerate(lista_frases_cruas):
 # print(lista_frases)
 frases_unidas = ', '.join(lista_frases)
 print(frases_unidas)
+
+##
+
+"""
+Lista de listas e seus índices
+"""
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
+
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][3])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
