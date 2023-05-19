e9dff7dcdbc246bd54bac80798e0b9c40538bece
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-05-19 16:59:43 -0300

    Exercicio CPF - Final
    Resolução do exercicio deCPF. Onde agora o cód é capaz de obter os 2 digitos do CPF e validar o CPF.

--- exercicios/exercicio_cpf.py
+++ exercicios/exercicio_cpf.py
@@ -29,15 +29,40 @@ contador_regressivo_1 = 10
 
 # Converte o digito para int e faz uma conta multiplicando cada digito por 10 e depois soma todos os resultados
 resultado_1 = 0
-for digito_1 in nove_digitos:
-    resultado_1 += int(digito_1) * contador_regressivo_1
+for digito in nove_digitos:
+    resultado_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -=1
 
 ''' Multiplica o resultado por 10 e pega o resto da divisão por 11.
     O resultado será o primeiro digito do CPF, caso o resultado seja maior ou igual a 9 o número será 0.
 '''
-
 digito_1 = (resultado_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 
 print(digito_1)
+
+# Obter o 2º digito do CPF #
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+# Converte o digito para int e faz uma conta multiplicando cada digito por 11 e depois soma todos os resultados
+resultado_2 = 0
+for digito in dez_digitos:
+    resultado_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -=1
+
+''' Multiplica o resultado por 10 e pega o resto da divisão por 11.
+    O resultado será o primeiro digito do CPF, caso o resultado seja maior ou igual a 9 o número será 0.
+'''
+digito_2 = (resultado_2 * 10) % 11
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+print(digito_2)
+
+# Faz a validação do CPF
+cpf_valido = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf == cpf_valido:
+    print(f'{cpf} é válido!')
+else:
+    print('CPF Inválido!')
